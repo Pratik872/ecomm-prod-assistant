919f06a9cb3246a005ae46b01ebaf250678ee2f3
Pratik Waghmare <waghmarepratik3@gmail.com>
2025-11-08 18:31:52 -0500

    data ingestion pipeline added

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -2,13 +2,14 @@ import os
 import sys
 import json
 from dotenv import load_dotenv
-from utils.config_loader import load_config
+from prod_assistant.utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-from logger import GLOBAL_LOGGER as log
-from exception.custom_exception import ProductAssistantException
+# from prod_assistant.logger import CustomLogger as log
+from prod_assistant.exception.custom_exception import ProductAssistantException
 import asyncio
+from langchain_google_vertexai import VertexAIEmbeddings
 
 
 class ApiKeyManager:
@@ -24,9 +25,9 @@ class ApiKeyManager:
         # Just log loaded keys (don't print actual values)
         for key, val in self.api_keys.items():
             if val:
-                log.info(f"{key} loaded from environment")
+                print((f"{key} loaded from environment"))
             else:
-                log.warning(f"{key} is missing from environment")
+                print((f"{key} is missing from environment"))
 
     def get(self, key: str):
         return self.api_keys.get(key)
@@ -39,7 +40,7 @@ class ModelLoader:
     def __init__(self):
         self.api_key_mgr = ApiKeyManager()
         self.config = load_config()
-        log.info("YAML config loaded", config_keys=list(self.config.keys()))
+        print("YAML config loaded")
 
     
 
@@ -49,7 +50,7 @@ class ModelLoader:
         """
         try:
             model_name = self.config["embedding_model"]["model_name"]
-            log.info("Loading embedding model", model=model_name)
+            print(f"Loading embedding model {model_name}")
 
             # Patch: Ensure an event loop exists for gRPC aio
             try:
@@ -57,12 +58,19 @@ class ModelLoader:
             except RuntimeError:
                 asyncio.set_event_loop(asyncio.new_event_loop())
 
-            return GoogleGenerativeAIEmbeddings(
-                model=model_name,
-                google_api_key=self.api_key_mgr.get("GOOGLE_API_KEY")  # type: ignore
-            )
+            # return GoogleGenerativeAIEmbeddings(
+            #     model=model_name,
+            #     google_api_key=self.api_key_mgr.get("GOOGLE_API_KEY")  # type: ignore
+            # )
+
+            return VertexAIEmbeddings(
+                model_name=model_name,
+                project="gen-lang-client-0579798402",
+                location="us-central1"
+                )
+        
         except Exception as e:
-            log.error("Error loading embedding model", error=str(e))
+            print(f"Error loading embedding model: {e}")
             raise ProductAssistantException("Failed to load embedding model", sys)
 
 
@@ -74,7 +82,7 @@ class ModelLoader:
         provider_key = os.getenv("LLM_PROVIDER", "openai")
 
         if provider_key not in llm_block:
-            log.error("LLM provider not found in config", provider=provider_key)
+            print("LLM provider not found in config")
             raise ValueError(f"LLM provider '{provider_key}' not found in config")
 
         llm_config = llm_block[provider_key]
@@ -83,7 +91,7 @@ class ModelLoader:
         temperature = llm_config.get("temperature", 0.2)
         max_tokens = llm_config.get("max_output_tokens", 2048)
 
-        log.info("Loading LLM", provider=provider, model=model_name)
+        print("Loading LLM")
 
         if provider == "google":
             return ChatGoogleGenerativeAI(
@@ -108,7 +116,7 @@ class ModelLoader:
             )
 
         else:
-            log.error("Unsupported LLM provider", provider=provider)
+            print(f"Unsupported LLM provider {provider}")
             raise ValueError(f"Unsupported LLM provider: {provider}")
 
 
